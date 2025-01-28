d4a5bf383455055c3a6d92b712ff3c1c65908aa9
Christmas <slinko.art@mail.ru>
2025-01-28 16:29:24 +0300

    test server-client implementation

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,5 +1,30 @@
-#include <iostream>
+#include "utils.h"
+#include "socket.h"
+
 
 int main() {
+    const Net::Address serverAddress = Net::Address::FromString("127.0.0.1", 8088);
+    std::cout << "Server address: " << serverAddress.ConvertToString() << std::endl;
+
+    Net::Socket clientSock(Net::Address::Family::IPv4, Net::Protocol::TCP);
+    LIBPOG_ASSERT(clientSock.IsOpen(), "Socket must be open");
+
+    bool result = clientSock.Connect(serverAddress);
+    LIBPOG_ASSERT(result == clientSock.IsConnected(), "Socket internal state must be the same");
+
+    if (clientSock.IsConnected() == false) {
+        std::cerr << "Socket is not connected. Exit." << std::endl;
+        return -1;
+    }
+
+    std::cout << "Client connected." << std::endl;
+
+    clientSock.Send("hello string");
+    clientSock.Close();
+
+    LIBPOG_ASSERT(!clientSock.IsConnected(), "Socket must be disconnected");
+    LIBPOG_ASSERT(!clientSock.IsOpen(), "Socket must be closed");
+
+    std::cout << "Done." << std::endl;
     return 0;
 }
--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,5 +1,38 @@
+#include "utils.h"
+#include "socket.h"
+
 #include <iostream>
 
+#define SERVER_PORT 8088
+
 int main() {
+    Net::Address bindAddress = Net::Address::MakeBind(SERVER_PORT);
+    LIBPOG_ASSERT(bindAddress.IsValid(), "Bind address must be valid");
+
+    Net::Socket serverSock(Net::Address::Family::IPv4);
+    LIBPOG_ASSERT(serverSock.IsOpen(), "Socket must be open");
+
+    Net::Address::port_t port = serverSock.Listen(bindAddress);
+    if (serverSock.IsListening() == false) {
+        std::cerr << "Socket is not listening. Exit." << std::endl;
+        return -1;
+    }
+
+    std::cout << "Socket is listening on port: " << port << std::endl;
+
+    Net::Socket sock = serverSock.Accept(bindAddress);
+
+    char buffer[16] = {0};
+    if (uint num = sock.Receive(buffer, sizeof(buffer))) {
+        std::cout << "Received " << num << " bytes: \"" << &buffer[0] << "\"." << std::endl;
+    } else {
+        std::cerr << "Failed to receive something." << std::endl;
+    }
+
+    serverSock.Close();
+    LIBPOG_ASSERT(serverSock.GetState() == Net::Socket::State::None, "Socket must be in `None` state");
+    LIBPOG_ASSERT(!serverSock.IsOpen(), "Socket must be closed");
+
+    std::cout << "Done." << std::endl;
     return 0;
 }
